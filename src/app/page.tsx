cf3e5fb245456ec2f5f8e198d1bd96f70b1ee893
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-12-07 13:41:10 -0400

    chore: remove logic from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,52 +4,33 @@ import { Formulario } from "@/components/Formulario";
 import { Layout } from "@/components/Layout";
 import { Tabela } from "@/components/Tabela";
 import { TableButton } from "@/components/TableButton";
-import Client from "@/core/Client";
-import { useState } from "react";
+import { useClients } from "@/hooks/useClients";
 
 export default function Home() {
-  const [visible, setVisible] = useState<'table' | 'form'>('table')
-  const [client, setClient] = useState<Client>(Client.vazio())
-
-  const clientes = [
-    new Client('Ana', 34, '1'),
-    new Client('João', 21, '2'),
-    new Client('Pedro', 49, '3'),
-    new Client('Karlos', 11, '4')
-  ]
-
-  function clienteSelecionado(cliente: Client) {
-    setClient(cliente)
-    setVisible('form')
-  }
-
-  function clienteExcluido(cliente: Client) {
-    console.log("Excluir", cliente.nome)
-  }
-
-  function novoCliente() {
-    setClient(Client.vazio())
-    setVisible('form')
-  }
-
-  function salvarCliente(cliente: Client) {
-    console.log(cliente)
-    setVisible('table')
-  }
+  const { 
+    clienteSelecionado, 
+    excluirCliente, 
+    novoCliente, 
+    salvarCliente, 
+    clients, 
+    client,
+    showTable,
+    visibleTable
+  } = useClients()
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples">
-        {visible === 'table' ? (
+        {visibleTable ? (
           <> 
             <div className="flex justify-end">
               <TableButton className="mb-4" onClick={novoCliente}>Novo Cliente</TableButton>
             </div>
 
-            <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+            <Tabela clientes={clients} clienteSelecionado={clienteSelecionado} clienteExcluido={excluirCliente}></Tabela>
           </>
         ): (
-          <Formulario client={client} onClick={() => setVisible('table')} changeClient={salvarCliente} /> 
+          <Formulario client={client} onClick={showTable} changeClient={salvarCliente} /> 
         )}
 
       </Layout>
