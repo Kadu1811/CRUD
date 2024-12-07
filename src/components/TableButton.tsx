a23d185943265b009b44001c0b02428e5a9ad50f
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-12-07 13:36:40 -0400

    chore: table button

--- src/components/TableButton.tsx
+++ src/components/TableButton.tsx
@@ -2,13 +2,17 @@ type ButtonProps = {
     color?: 'green' | 'blue' | 'gray'
     className?: string
     children: any
+    onClick?: () => void
 }
 
-const TableButton = ({color, className, children}: ButtonProps) => {
+const TableButton = ({color, className, children, onClick}: ButtonProps) => {
     const cor = color ?? 'green'
 
     return (
-        <button className={`bg-gradient-to-r from-${cor}-400 to-${cor}-700 text-white px-4 py-2 rounded-md ${className}`}>
+        <button 
+            className={`bg-gradient-to-r from-${cor}-400 to-${cor}-700 text-white px-4 py-2 rounded-md ${className}`}
+            onClick={onClick}
+        >
             {children}
         </button>
     )
